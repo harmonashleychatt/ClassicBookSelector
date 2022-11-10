8196b127a7e83c095b10aa5d27ecc36fb6c525ee
harmonashleychatt <102543767+harmonashleychatt@users.noreply.github.com>
2022-11-09 19:12:06 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace ClassicBookSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Dubai", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Items.AddRange(new object[] {
+            "1. Pete the Cat and His Four Groovy Buttons",
+            "2. The Secret Language of Cats",
+            "3. How to Be a Cat",
+            "4. Gumpy Cat: A Grumpy Book ",
+            "5. How to Talk to Your Cat About Gun Safety, Abstinence, Drugs, Satanism, And Oth" +
+                "er Dangers That Threaten Their Nine Lives"});
+            this.listBox1.Location = new System.Drawing.Point(33, 170);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(709, 130);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseLeave += new System.EventHandler(this.listBox1_MouseLeave);
+            this.listBox1.MouseHover += new System.EventHandler(this.listBox1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Copperplate Gothic Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(95, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseHover += new System.EventHandler(this.listBox1_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(33, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Create a project named ClassicBookSelector that contains a Form with a ListBox that lists at least five classic books that you think all educated
+//people should have read. When the user places the mouse over the ListBox, display a Label that contains a general statement about the benefits 
+//of reading. The Label disappears when the user’s mouse leaves the ListBox area. When the user clicks a book title in the ListBox, display another 
+//Label that contains a brief synopsis of the specific book. Also, change the BackColor of the Form to a different color for each book.
+namespace ClassicBookSelector
+{
+    public partial class Form1 : Form
+    {
+        string a = "Expand your knowledge by reading important material!";
+        public bool t = true;
+        string zero = "Pete the Cat is wearing his favorite shirt- the one with four totally groovy \nbuttons. But when one falls off, does Pete cry?";
+        string one = "Cats do not meow randomly, nor do they growl or hiss because they have nothing better to do. Cat sounds have \na purpose, and they can carry important messages, whether for us or other cats.";
+        string two = "A single word of text per spread teaches readers “how to be a cat”―how to stretch, clean, pounce, feast―while \nthe striking paper cuts illustrate the kitten's attempts to imitate an adult cat's \nmastery of each skill.";
+        string three = "Internet sensation Grumpy Cat's epic feline frown has inspired legions of devoted fans. Celebrating the grouch \nin everyone,the Grumpy Cat book teaches the fine art of grumpiness and includes \nenough bad attitude to cast a dark cloud over the whole world.";
+        string four = "Long gone are the good old days when a cat’s biggest worries were mean dogs or a bath. Modern cats \nmust confront satanists, online predators, the possibility of needing to survive in a post-apocalyptic \nwasteland, and countless other threats to their nine lives.";
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Enabled = false;
+            label1.Text = "";
+            label1.Text = "";
+            label2.Enabled = false;
+
+     
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseHover(object sender, EventArgs e)
+        {
+            label1.Enabled = true;
+            label1.Text = a;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Enabled = false;
+            label1.Text = "";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex % 5 == 0)
+            {
+                this.BackColor = Color.AliceBlue;
+                label2.Enabled = true;
+                label2.Text = zero;
+            }
+            else if (listBox1.SelectedIndex % 5 == 1)
+            {
+                this.BackColor = Color.SlateBlue;
+                label2.Enabled = true;
+                label2.Text = one;
+            }
+            else if (listBox1.SelectedIndex % 5 == 2)
+            {
+                this.BackColor = Color.SteelBlue;
+                label2.Enabled = true;
+                label2.Text = two;
+            }
+            else if (listBox1.SelectedIndex % 5 == 3)
+            {
+                this.BackColor = Color.LightSkyBlue;
+                label2.Enabled = true;
+                label2.Text = three;
+            }
+            else if (listBox1.SelectedIndex % 5 == 4)
+            {
+                this.BackColor = Color.CornflowerBlue;
+                label2.Enabled = true;
+                label2.Text = four;
+            }
+        }
+    }
+}
